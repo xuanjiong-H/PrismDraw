b5e82f716f76d9f0b9bcf7df1833a789974b68c0
xuanjiong-H <bdhc_huang@163.com>
2026-05-19 16:03:01 +0800

    feat：领取活动扣减账户额度

--- big-market-domain/src/main/java/cn/bugstack/domain/activity/model/entity/ActivityAccountMonthEntity.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/activity/model/entity/ActivityAccountMonthEntity.java
@@ -1,25 +1,21 @@
-package cn.bugstack.infrastructure.persistent.po;
+package cn.bugstack.domain.activity.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
- * @description 抽奖活动账户表-月次数
- * @create 2024-04-03 15:28
+ * @description 活动账户（月）实体对象
+ * @create 2024-04-05 08:38
  */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RaffleActivityAccountMonth {
+public class ActivityAccountMonthEntity {
 
-    /** 自增ID */
-    private String id;
     /** 用户ID */
     private String userId;
     /** 活动ID */
@@ -30,9 +26,5 @@ public class RaffleActivityAccountMonth {
     private Integer monthCount;
     /** 月次数-剩余 */
     private Integer monthCountSurplus;
-    /** 创建时间 */
-    private Date createTime;
-    /** 更新时间 */
-    private Date updateTime;
 
 }
--- big-market-domain/src/main/java/cn/bugstack/domain/activity/model/entity/UserRaffleOrderEntity.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/activity/model/entity/UserRaffleOrderEntity.java
@@ -1,5 +1,6 @@
-package cn.bugstack.infrastructure.persistent.po;
+package cn.bugstack.domain.activity.model.entity;
 
+import cn.bugstack.domain.activity.model.valobj.UserRaffleOrderStateVO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,17 +10,15 @@ import java.util.Date;
 
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
- * @description 用户抽奖订单表
- * @create 2024-04-03 15:30
+ * @description 用户抽奖订单实体对象
+ * @create 2024-04-04 18:53
  */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserRaffleOrder {
+public class UserRaffleOrderEntity {
 
-    /** 用户ID */
-    private String id;
     /** 活动ID */
     private String userId;
     /** 活动名称 */
@@ -33,10 +32,6 @@ public class UserRaffleOrder {
     /** 订单状态；create-创建、used-已使用、cancel-已作废 */
     private Date orderTime;
     /** 创建时间 */
-    private String orderState;
-    /** 更新时间 */
-    private Date createTime;
-    /**  */
-    private Date updateTime;
+    private UserRaffleOrderStateVO orderState;
 
 }
