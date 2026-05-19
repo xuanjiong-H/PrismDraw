2dfe550d9df8d0a22d574ad68802204002bfb90c
xuanjiong-H <bdhc_huang@163.com>
2026-05-19 14:17:36 +0800

    feat：用于领取活动库表设计

---
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IRaffleActivityAccountMonthDao.java
@@ -0,0 +1,9 @@
+package cn.bugstack.infrastructure.persistent.dao;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖活动账户表-月次数
+ * @create 2024-04-03 15:57
+ */
+public interface IRaffleActivityAccountMonthDao {
+}
